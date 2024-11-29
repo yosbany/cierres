f43c9d63fff01713b49ae5926329d5b4b1cf4298
yosbany <ytejas.86@gmail.com>
2024-11-29 19:28:39 -0300

    Varios OK

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    base: './',
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 100
+      },
+      hmr: {
+        overlay: true
+      }
     },
-    hmr: {
-      overlay: true
+    optimizeDeps: {
+      exclude: ['lucide-react']
+    },
+    define: {
+      'process.env': env
     }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react']
-  }
+  };
 });
